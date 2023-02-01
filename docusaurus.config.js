48d4ca070b3194044655c4759339f5776e5cfaee
GemInee <gladyshevpv@gmail.com>
2023-02-01 09:15:37 +0700

    Edit this page button not working #144
    - Fix url path to github code branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Unitystation-fork/Unitystation-WikiV2',
+            'https://github.com/Unitystation-fork/Unitystation-WikiV2/tree/main',
         },
         blog: {
           showReadingTime: true,
